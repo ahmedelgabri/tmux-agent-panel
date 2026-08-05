b8350933619aa20322af693f9788d0dda854e88d
Ahmed El Gabri <ahmed@gabri.me>
2026-08-05 10:16:13 +0200

    chore: release v0.1.1
    Ships the installer fixes: symlinked configs survive install/uninstall
    and hooks invoke tap from PATH.
    
    Change-Id: I6862a239ab04beb9a3fc1c8bd3129dca6a6a6964

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
           default = self'.packages.tap;
           tap = pkgs.buildGoModule {
             pname = "tap";
-            version = "0.1.0";
+            version = "0.1.1";
 
             src = lib.cleanSource ./.;
 
