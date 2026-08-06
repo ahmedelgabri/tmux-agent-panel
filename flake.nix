02ade96ae96276fa14fe604eb529d8d586969757
Ahmed El Gabri <ahmed@gabri.me>
2026-08-06 18:14:12 +0200

    chore: release v0.1.6
    Removes the thick default fzf gutter beside picker rows while preserving the focused-row pointer.

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
           default = self'.packages.tap;
           tap = pkgs.buildGoModule {
             pname = "tap";
-            version = "0.1.5";
+            version = "0.1.6";
 
             src = lib.cleanSource ./.;
 
