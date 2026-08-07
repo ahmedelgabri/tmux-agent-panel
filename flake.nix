c206b45572d8fc3ff068b829f7959f3d0789ccdf
Ahmed El Gabri <ahmed@gabri.me>
2026-08-07 11:00:50 +0200

    chore: release v0.1.8
    Change-Id: I7c26f23ed02a6469e00df41cca16d2746a6a6964

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
           default = self'.packages.tap;
           tap = pkgs.buildGoModule {
             pname = "tap";
-            version = "0.1.7";
+            version = "0.1.8";
 
             src = lib.cleanSource ./.;
 
