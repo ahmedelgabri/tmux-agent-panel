570e7c26f8b11fa1a76cd4c775c691247ac653d2
Ahmed El Gabri <ahmed@gabri.me>
2026-09-09 12:26:53 +0200

    chore: prepare v0.2.1
    Bump the version in flake.nix and synchronize the pi package and plugin
    manifests for the build-time fzf patch release.
    
    Change-Id: I48f36c39d66a2fd0a77c650b1bf1d9226a6a6964

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
           default = self'.packages.tap;
           tap = pkgs.buildGoModule {
             pname = "tap";
-            version = "0.2.0";
+            version = "0.2.1";
 
             src = lib.cleanSource ./.;
 
